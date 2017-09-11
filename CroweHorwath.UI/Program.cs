18bb6575d4b47a62247bbdf50ce8df58b5e4eec7
robert <perryrobert410@gmail.com>
2017-09-11 23:23:16 +0500

    Initial commit

---
+++ CroweHorwath.UI/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CroweHorwath.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Clear();
+            DateTime dat = DateTime.Now;
+            Console.WriteLine("\nToday is {0:d} at {0:T}.", dat);
+            Console.Write("\nPress H to print static string using WCF services. ");
+            var enteredString = Console.ReadLine();
+
+            while (enteredString.ToLower() != "h")
+            {
+                Console.Write("\nYou entered another char. ");
+                Console.Write("\nPress H to print static string using WCF services. ");
+                enteredString = Console.ReadLine();
+            }
+
+            CroweHorwathService.ICHService objService = new CroweHorwathService.CHServiceClient();
+            var result = objService.GetData();
+            Console.WriteLine(result);
+
+            Console.Write("\nPress any string and hit enter to print dynamic string using WCF services. ");
+            enteredString = Console.ReadLine();
+            result = objService.GetUserData(enteredString);
+            Console.WriteLine(result);
+
+            Console.ReadLine();
+        }
+    }
+}
